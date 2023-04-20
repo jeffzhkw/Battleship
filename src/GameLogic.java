0d24e4c25ca4c3334f8fdb9abd18cf4276eb6252
Kewei Zhang <jeffzhkw@gmail.com>
2023-04-19 20:18:31 -0400

    Update

---
+++ src/GameLogic.java
@@ -0,0 +1,17 @@
+public class GameLogic {
+    Grid p1;
+    Grid p2;
+
+    boolean isP1Move = true;
+
+
+
+    public GameLogic(Grid p1, Grid p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public void start(){
+
+    }
+}
